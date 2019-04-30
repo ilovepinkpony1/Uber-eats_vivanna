881ee11f5539e41e6456cfbda5578dbd89492094
Vitalii <ilovepinkpony1@gmail.com>
2019-04-30 22:32:30 +0300

    added-current-buy

--- src/script.js
+++ src/script.js
@@ -1,11 +1,30 @@
 'use stirct';
 
  window.onscroll = () => {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    const button = document.getElementById('scrollUp');
-    if (scrolled < 300) {
-        button.style.display = 'none';
-    } else {
-        button.style.display = 'flex';
-    }
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  const button = document.getElementById('scrollUp');
+  if (scrolled < 300) {
+    button.style.display = 'none';
+  } else {
+    button.style.display = 'flex';
   }
+}
+
+
+let currentBuys = 0;
+document.onclick = function(event){
+  const buys = document.getElementById('currentBuys');
+
+  if (
+  event.target.className === 'restaurant' 
+  || event.target.className === 'restImg' 
+  || event.target.className === 'restaurantName' 
+  || event.target.className === 'foodType' 
+  || event.target.className === 'time'
+  ) {
+    currentBuys += 1;
+    buys.style.display = 'flex';
+    buys.innerHTML = `${currentBuys}`;
+  };
+}
+    
